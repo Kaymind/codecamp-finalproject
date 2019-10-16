75961c451eecb6b90dc8b99446454d4cc52eafc3
Kaymind <lempelziv@gmail.com>
2019-10-16 20:11:46 +0700

    dynamic page

--- main.js
+++ main.js
@@ -25,3 +25,60 @@ const run = () => {
 window.addEventListener('load', run);
 window.addEventListener('resize', run);
 window.addEventListener('scroll', run);
+
+
+// Edit name & nickname
+const addForm = document.querySelector('.add');
+
+addForm.name.value = document.querySelector('.fullname').textContent;
+
+addForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const name = addForm.name.value;
+    const mainColor = addForm.color1.value;
+    const fontColor = addForm.color3.value;
+    document.querySelector('#main-header').style.backgroundColor = mainColor;
+    document.querySelector('body').style.color = fontColor;
+    document.querySelector('.fullname').innerHTML = name;
+});
+
+// Show/Hide edit form
+const editBtn = document.querySelector('.edit');
+
+editBtn.addEventListener('click', e => {
+    addForm.classList.toggle('hide');
+});
+
+// Add skills/abilities
+const skillForm = document.querySelector('.add-skill');
+const list = document.querySelector('.skill-items');
+
+const generateTemplate = skill => {
+    const html = `<li>${skill} <i class="far fa-trash-alt delete"></li>`;
+    list.innerHTML += html;
+}
+
+skillForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const skill = skillForm.skill.value.trim();
+
+    if(skill.length){
+        generateTemplate(skill);
+        skillForm.reset();
+    }
+});
+
+// Delete skills/abilities
+list.addEventListener('click', e => {
+    if(e.target.classList.contains('delete')){
+        e.target.parentElement.remove();
+    }
+});
+
+// Show/Hide edit form
+const editSkill = document.querySelector('.edit-skill');
+
+editSkill.addEventListener('click', e => {
+    skillForm.classList.toggle('hide');
+});
